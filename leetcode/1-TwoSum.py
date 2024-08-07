8dde48a9440d2071648317e49b78c8a9dc02a352
Gitesh1998 <giteshpawar56@gmail.com>
2024-08-06 19:47:02 -0700

    Happy Number leetcode problem solved

--- leetcode/1-TwoSum.py
+++ leetcode/1-TwoSum.py
@@ -5,7 +5,6 @@ class Solution:
             if target - val in dict:
                 return [dict[target - val], ind]
             dict[val] = ind
-        
         return True
     
 nums = [2,7,11,15]
---
+++ leetcode/202-HappyNumber.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        dict = {}
+        while True:
+            strs = str(n)
+            sums = 0
+            for i in strs:
+                sums += (int(i)*int(i))    
+            print(n, sums)
+            if sums == 1:
+                return True
+            if sums in dict:
+                return False
+            n = sums
+            dict[sums] = True
+
+n = 19
+print(Solution().isHappy(n))
