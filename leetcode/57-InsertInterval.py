8a9503501ad2e62a334d3e719c46c66e7fc27544
Gitesh1998 <giteshpawar56@gmail.com>
2024-08-14 12:55:55 -0700

    Added Insert Interval Leetcode problem

--- leetcode/57-InsertInterval.py
+++ leetcode/57-InsertInterval.py
@@ -1,19 +1,44 @@
 class Solution:
-    def insert(self, intervals: list[list[int]], newInterval: list[int]) -> list[list[int]]:
-    
+    def insert(
+        self, intervals: list[list[int]], newInterval: list[int]
+    ) -> list[list[int]]:
+        if len(intervals) == 0:
+            return [newInterval]
+
         if newInterval[1] < intervals[0][0]:
             return [newInterval, *intervals]
-    
+
         if newInterval[0] > intervals[-1][1]:
             return [*intervals, newInterval]
-    
-    
-        start = newInterval[0] if newInterval[0] < intervals[0][0] else intervals[0][0]
-        print(start)
-    
-        return True
-    
-    
-intervals = [[1,3],[6,9]]
+
+        result = []
+        i = 0
+        while i < len(intervals):
+            if (intervals[i][0] <= newInterval[0] <= intervals[i][1]) or (intervals[i][0] <= newInterval[1] <= intervals[i][1]) or (newInterval[0] <= intervals[i][0] <= newInterval[1]): 
+                start = intervals[i][0] if intervals[i][0] < newInterval[0] else newInterval[0]
+                end = intervals[i][1] if intervals[i][1] > newInterval[1] else newInterval[1]
+                i+=1
+                while i < len(intervals) and intervals[i][0] <= end:
+                    end = intervals[i][1] if intervals[i][1] > end else end
+                    i+=1
+                result.append([start, end])
+                return [*result, *intervals[i:]]
+            elif newInterval[1] < intervals[i][0]:
+                return [*result, newInterval, *intervals[i:]]
+            
+            result.append(intervals[i])
+            i+=1
+            
+            
+        return result
+
+
+intervals = [[1, 3], [6, 9]]
 newInterval = [2, 5]
+intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]]
+newInterval = [4,8]
+
+intervals = [[1,5]]
+newInterval = [0,6]
+
 print(Solution().insert(intervals, newInterval))
