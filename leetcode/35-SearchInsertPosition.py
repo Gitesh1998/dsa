9ae972ce53b3294ebf21b6082c858b7bf8fa51a1
Gitesh1998 <giteshpawar56@gmail.com>
2024-08-29 19:38:32 -0700

    Solved leetcode Search Insert Position problem

---
+++ leetcode/35-SearchInsertPosition.py
@@ -0,0 +1,18 @@
+class Solution:
+    def searchInsert(self, nums: list[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return left
+    
+
+   
+nums = [1, 3]
+target = 0
+print(Solution().searchInsert(nums, target))
---
+++ leetcode/918-MaximumSumCircularSubarray.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxSubarraySumCircular(self, nums: list[int]) -> int:
+        return True
+
+nums = [1, -2, 3, -2]
+nums = [-2,1,-3,4,-1,2,1,-5,4]
+nums = [5,-3,5]
+print(Solution().maxSubarraySumCircular(nums))
